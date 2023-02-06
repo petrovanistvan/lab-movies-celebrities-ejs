ca262f769035c415d9c8b2a38432da7cfb210b51
Istvan Petrovan <petrovanistvan@gmail.com>
2023-02-06 17:04:28 +0100

    third iteration

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -3,4 +3,22 @@ const router = require('express').Router()
 
 // all your routes here
 
+router.get("/create", (req, res, next) => {
+    res.render("celebrities/new-celebrity");
+  });
+
+router.post("/create", (req, res) => {
+    console.log(req.body)
+  });
+
 module.exports = router
+
+router.post("/create", async (req, res) => {
+
+    try{
+      const body = await req.body
+      res.redirect("/celebrities")
+    } catch (error) {
+      res.render("celebrities/new-celebrity");
+    }
+  })
